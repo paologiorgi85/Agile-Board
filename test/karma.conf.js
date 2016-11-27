5ecc2454c1db42fc225ce42e3a44b46a2cc5c593
paolo <paolo.giorgi85@gmail.com>
2016-11-27 23:07:06 +0100

    update it

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
+      'bower_components/angular-dragula/dist/angular-dragula.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
