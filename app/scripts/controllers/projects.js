326fd114611940cc3b18da60e812a672580998de
paolo <paolo.giorgi85@gmail.com>
2017-01-02 16:40:57 +0100

    Add files

---
+++ app/scripts/controllers/projects.js
@@ -0,0 +1,25 @@
+myApp.controller('projectsListCtrl', ['ProjectService', '$scope','projects', function(ProjectService, $scope, projects, $http) {
+  $scope.projects = projects;
+  //ProjectService.getAllProjects($scope);
+/*
+  $scope.select = function(id) {
+    console.log("Into Select projcet function");
+    for(i in $scope.projects) {
+      if($scope.projects[i].id == id) {
+        //we use angular.copy() method to create
+        //copy of original object
+        $scope.project = angular.copy($scope.projects[i]);
+      }
+    }
+  }
+*/
+}]);
+
+myApp.controller('projectDetailCtrl', ['ProjectService', '$scope', '$http','$stateParams', '$state', function(ProjectService, $scope, $http, $stateParams, $state) {
+  console.log("List of Projects into projectDetailCtrl:");
+  $scope.projectList = ProjectService.getAllProjects();
+  console.log($scope.projectList);
+  console.log("Value of $state.params['projectID']:");
+  console.log($state.params['projectID']);
+  $scope.project = $scope.projectList[1];
+}]);
---
+++ app/scripts/service/project.js
@@ -0,0 +1,14 @@
+myApp.service('ProjectService', function($http, $state) {
+  var service = {
+
+    getAllProjects: function() {
+      console.log("Service - function: getAllProjects()");
+      return $http.get('data/projects.json', { cache: true }).then(function(res) {
+        //console.log(res.data);
+        return res.data;
+      })
+    },
+
+  }
+  return service;
+})
